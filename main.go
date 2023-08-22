c1fc761a3b5824782a140aa32f1a36d3898301eb
miguelZapat <103950043+miguelZapat@users.noreply.github.com>
2023-08-22 12:26:10 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"os"
+)
+
+func Tree() {
+
+    
+	root := "1_.txt"
+	jsonData := make(map[string]interface{})
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println("Error walking directory: ", err)
+			return err
+		}
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".txt") {
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				fmt.Println("Error reading file: ", err)
+				return err
+			}
+			fileData := map[string]interface{}{
+				"content": string(data),
+			}
+			jsonData[info.Name()] = fileData
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println("Error walking directory: ", err)
+		return
+	}
+
+	//jsonStr, err := json.MarshalIndent(jsonData, "", "  ")
+	jsonStr, err := json.Marshal(jsonData)
+	if err != nil {
+		fmt.Println("Error converting to JSON: ", err)
+		return
+	}
+
+	fmt.Println(string(jsonStr))
+}
+
+func main() {
+	Tree()
+}
